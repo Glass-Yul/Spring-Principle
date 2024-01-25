9c8961f6cc1cb36b125e040d988a60e898b3826c
Glass <dbfl3742@naver.com>
2024-01-25 17:09:37 +0900

    의존관계 자동주입
    스프링 부트가 아니라 내가 직접 기술 지원 객체를 스프링 빈으로 등록한다면 수동으로 등록해서 명확하게 드러내는 것이 좋다.

---
+++ core/src/main/java/hello/core/singleton/SingletonServiceTest.java
@@ -0,0 +1,11 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonServiceTest {
+
+
+}
